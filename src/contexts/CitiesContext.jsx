3a42fb335832e8ded8a1043523d58a500b8449b3
Magno <jmagno.dutra@hotmail.com>
2024-12-19 21:04:39 -0300

    227 - creating a citiesContext

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,44 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:9000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function getCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        console.log(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+
+  if (context === undefined) throw new Error("Context out of bound");
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
